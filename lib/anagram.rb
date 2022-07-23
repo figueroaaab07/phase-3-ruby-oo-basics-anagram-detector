76af571bf6c9d38b2bab60fba37a1e960034cf8c
Alejandro Luis Figueroa Cevallos <figueroaaab07@gmail.com>
2022-07-22 22:16:23 -0700

    Completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  attr_reader :word
+  attr_accessor :array
+  def initialize(word)
+    @word = word
+  end
+  def match(array)
+    @array = array
+    @array.filter do |element|
+      element.chars.sort == @word.chars.sort
+    end
+  end
+end
